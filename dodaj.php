9d33c15edf8b2a87dabc261c17687ed95d47118f
Kurczak <patryczek2406@gmail.com>
2018-03-25 14:51:05 +0200

    dodaj

--- dodaj.php
+++ dodaj.php
@@ -14,18 +14,35 @@ session_start();
     {
    
     
-        $imie = $_POST['imie'];
-        $nazwisko = $_POST['nazwisko'];
+        
         $tekst = $_POST['tekst'];
         
+         $sql = "INSERT INTO aktualnosci (id_ucznia)
+                SELECT id_ucznia
+                FROM uczniowie";
+                    $wykonaj = $polaczenie->query($sql);
+        
+        $sql3 = "INSERT INTO aktualnosci (tekst) VALUES ('$tekst')" or die(mysql_error());
+         $wykonaj = $polaczenie->query($sql3);  
+         
+          $sql2="SELECT uczniowie.imie_ucznia, uczniowie.nazwisko_ucznia
+				FROM uczniowie
+				INNER JOIN aktualnosci ON uczniowie.id_ucznia = aktualnosci.id_ucznia";
+        $wykonaj = $polaczenie->query($sql2);
+        
+       
  
-        $sql="SELECT * FROM uczniowie AND nauczyciele WHERE imie='$imie' AND nazwisko='$nazwisko' ";
+        $imie = $_POST['imie_ucznia'];
+        $nazwisko = $_POST['nazwisko_ucznia'];
+       
+      
         
-        //$zapytanie = "INSERT INTO aktualnosci (imie, nazwisko, tekst) VALUES ('$imie','$nazwisko','$tekst')" or die(mysql_error());
+       // $sql3 = "INSERT INTO aktualnosci (imie, nazwisko, tekst) VALUES ('$imie','$nazwisko','$tekst')" or die(mysql_error());
         
-        $wykonaj = $polaczenie->query($sql);
-     // $wykonaj = $polaczenie->query($zapytanie);
-        header('Location:aktualnosci.php');
+  
+     
+              
+        header('Location:aktualnosci_ucznia.php');
     }
  
 $polaczenie->close();    
