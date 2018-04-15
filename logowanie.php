93b2d3e1cb40c4952d6d6c0080844a176e0f4985
Mateusz <mederill@gmail.com>
2018-04-15 20:26:24 +0200

    widzi cssa

--- logowanie.php
+++ logowanie.php
@@ -10,7 +10,7 @@ session_start();
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title> EDziennik </title>
 
-<link rel="stylesheet" href="CSS/stylelogowanie.css"/>
+<link rel="stylesheet" href="stylelogowanie.css"/>
 
 </head>
 <body>
