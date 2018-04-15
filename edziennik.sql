daa8815ae1bb61c2f0c9372462c96ed0128a3087
maxiormxr <mxr0zsk@gmail.com>
2018-04-15 12:33:06 +0200

    dodanie plany.php
    plik plany.php wyswietla plan zajec dla konkretnego ucznia, w bazie dodano tabele uczen.

--- edziennik.sql
+++ edziennik.sql
@@ -3,11 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
--- Czas generowania: 09 Kwi 2018, 10:21
-=======
--- Czas generowania: 14 Kwi 2018, 19:48
->>>>>>> Patryk
+-- Czas generowania: 14 Kwi 2018, 20:06
 -- Wersja serwera: 10.1.31-MariaDB
 -- Wersja PHP: 7.2.3
 
@@ -55,8 +51,6 @@ CREATE TABLE `daneuzytkownika` (
   `miasto` varchar(25) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `pesel` varchar(11) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-<<<<<<< HEAD
-=======
 
 --
 -- Zrzut danych tabeli `daneuzytkownika`
@@ -68,7 +62,6 @@ INSERT INTO `daneuzytkownika` (`id_uzytkownika`, `imie`, `nazwisko`, `adress`, `
 (6, 'Łukasz', 'Szorc', 'Sosnowiec', '8', 'Sosnowiec', '000411'),
 (7, 'Adam', 'Nowak', 'jakis', 'inny', 'jakies', '959884'),
 (8, 'Mateusz', 'Machcinski', 'asfa', '111', 'asfsdf', '2123');
->>>>>>> Patryk
 
 -- --------------------------------------------------------
 
@@ -80,15 +73,6 @@ CREATE TABLE `klasa` (
   `id_klasy` int(11) NOT NULL,
   `nazwa` varchar(11) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-<<<<<<< HEAD
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `nauczyciele_klasa_przedmiot`
---
-
-=======
 
 --
 -- Zrzut danych tabeli `klasa`
@@ -104,16 +88,10 @@ INSERT INTO `klasa` (`id_klasy`, `nazwa`) VALUES
 -- Struktura tabeli dla tabeli `nauczyciele_klasa_przedmiot`
 --
 
->>>>>>> Patryk
 CREATE TABLE `nauczyciele_klasa_przedmiot` (
   `id_klasa` int(11) NOT NULL,
   `id_użytkownika` int(11) DEFAULT NULL,
   `id_przedmiotu` int(11) DEFAULT NULL,
-<<<<<<< HEAD
-  `id_oceny` int(11) NOT NULL,
-  `id_klasy` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-=======
   `id_klasy` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -124,7 +102,6 @@ CREATE TABLE `nauczyciele_klasa_przedmiot` (
 INSERT INTO `nauczyciele_klasa_przedmiot` (`id_klasa`, `id_użytkownika`, `id_przedmiotu`, `id_klasy`) VALUES
 (1, 5, 1, 1),
 (2, 8, 2, 2);
->>>>>>> Patryk
 
 -- --------------------------------------------------------
 
@@ -137,12 +114,8 @@ CREATE TABLE `oceny` (
   `id_przemiotu` int(11) NOT NULL,
   `ocena` int(11) NOT NULL,
   `id_uzytkownika` int(11) NOT NULL,
-<<<<<<< HEAD
-  `waga` text COLLATE utf8_polish_ci NOT NULL
-=======
   `waga` text COLLATE utf8_polish_ci NOT NULL,
   `id_klasy` int(11) NOT NULL
->>>>>>> Patryk
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
@@ -159,14 +132,6 @@ INSERT INTO `oceny` (`id_oceny`, `id_przemiotu`, `ocena`, `id_uzytkownika`, `wag
 
 --
 -- Struktura tabeli dla tabeli `przedmioty`
-<<<<<<< HEAD
---
-
-CREATE TABLE `przedmioty` (
-  `id_przedmiotu` int(11) NOT NULL,
-  `nazwa` varchar(11) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-=======
 --
 
 CREATE TABLE `przedmioty` (
@@ -181,7 +146,6 @@ CREATE TABLE `przedmioty` (
 INSERT INTO `przedmioty` (`id_przedmiotu`, `nazwa`) VALUES
 (1, 'matematyka'),
 (2, 'polski');
->>>>>>> Patryk
 
 -- --------------------------------------------------------
 
@@ -193,30 +157,20 @@ CREATE TABLE `uzytkownicy` (
   `id_uzytkownika` int(11) NOT NULL,
   `login` varchar(30) COLLATE utf8_polish_ci NOT NULL,
   `haslo` varchar(20) COLLATE utf8_polish_ci NOT NULL,
-<<<<<<< HEAD
-  `Rola` int(2) NOT NULL
-=======
   `rola` varchar(5) COLLATE utf8_polish_ci NOT NULL,
   `rodzaj` varchar(40) COLLATE utf8_polish_ci NOT NULL
->>>>>>> Patryk
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Zrzut danych tabeli `uzytkownicy`
 --
 
-<<<<<<< HEAD
-INSERT INTO `uzytkownicy` (`id_uzytkownika`, `login`, `haslo`, `Rola`) VALUES
-(1, 'pat', 'qaz', 0),
-(5, 'mat', 'qaz', 0);
-=======
 INSERT INTO `uzytkownicy` (`id_uzytkownika`, `login`, `haslo`, `rola`, `rodzaj`) VALUES
 (1, 'patkur', 'qaz', '1', 'Uczen'),
 (5, 'matlec', 'qaz', '2', 'Nauczyciel'),
 (6, 'lukszo', 'qaz', '1', 'Uczen'),
 (7, 'adanow', 'now', '1', 'Uczen'),
 (8, 'matmac', 'mac', '2', 'Nauczyciel');
->>>>>>> Patryk
 
 --
 -- Indeksy dla zrzutów tabel
@@ -228,19 +182,6 @@ INSERT INTO `uzytkownicy` (`id_uzytkownika`, `login`, `haslo`, `rola`, `rodzaj`)
 ALTER TABLE `aktualnosci`
   ADD PRIMARY KEY (`id_aktualnosci`),
   ADD KEY `Id_uzytkownika` (`id_uzytkownika`);
-<<<<<<< HEAD
-
---
--- Indeksy dla tabeli `daneuzytkownika`
---
-ALTER TABLE `daneuzytkownika`
-  ADD PRIMARY KEY (`id_uzytkownika`),
-  ADD KEY `id_uzytkownika` (`id_uzytkownika`);
-
---
--- Indeksy dla tabeli `klasa`
---
-=======
 
 --
 -- Indeksy dla tabeli `daneuzytkownika`
@@ -251,7 +192,6 @@ ALTER TABLE `daneuzytkownika`
 --
 -- Indeksy dla tabeli `klasa`
 --
->>>>>>> Patryk
 ALTER TABLE `klasa`
   ADD PRIMARY KEY (`id_klasy`);
 
@@ -261,12 +201,7 @@ ALTER TABLE `klasa`
 ALTER TABLE `nauczyciele_klasa_przedmiot`
   ADD PRIMARY KEY (`id_klasa`),
   ADD KEY `id_klasy` (`id_klasy`),
-<<<<<<< HEAD
-  ADD KEY `id_użytkownika` (`id_użytkownika`,`id_przedmiotu`,`id_oceny`),
-  ADD KEY `id_oceny` (`id_oceny`),
-=======
   ADD KEY `id_użytkownika` (`id_użytkownika`,`id_przedmiotu`),
->>>>>>> Patryk
   ADD KEY `id_przedmiotu` (`id_przedmiotu`);
 
 --
@@ -275,12 +210,8 @@ ALTER TABLE `nauczyciele_klasa_przedmiot`
 ALTER TABLE `oceny`
   ADD PRIMARY KEY (`id_oceny`),
   ADD KEY `id_przemiotu` (`id_przemiotu`),
-<<<<<<< HEAD
-  ADD KEY `id_uzytkownika` (`id_uzytkownika`);
-=======
   ADD KEY `id_uzytkownika` (`id_uzytkownika`),
   ADD KEY `id_klasy` (`id_klasy`);
->>>>>>> Patryk
 
 --
 -- Indeksy dla tabeli `przedmioty`
@@ -305,12 +236,6 @@ ALTER TABLE `aktualnosci`
   MODIFY `id_aktualnosci` int(11) NOT NULL AUTO_INCREMENT;
 
 --
-<<<<<<< HEAD
--- AUTO_INCREMENT dla tabeli `oceny`
---
-ALTER TABLE `oceny`
-  MODIFY `id_oceny` int(11) NOT NULL AUTO_INCREMENT;
-=======
 -- AUTO_INCREMENT dla tabeli `nauczyciele_klasa_przedmiot`
 --
 ALTER TABLE `nauczyciele_klasa_przedmiot`
@@ -321,7 +246,6 @@ ALTER TABLE `nauczyciele_klasa_przedmiot`
 --
 ALTER TABLE `oceny`
   MODIFY `id_oceny` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
->>>>>>> Patryk
 
 --
 -- AUTO_INCREMENT dla tabeli `uzytkownicy`
@@ -350,10 +274,6 @@ ALTER TABLE `daneuzytkownika`
 --
 ALTER TABLE `nauczyciele_klasa_przedmiot`
   ADD CONSTRAINT `nauczyciele_klasa_przedmiot_ibfk_1` FOREIGN KEY (`id_klasy`) REFERENCES `klasa` (`id_klasy`) ON DELETE CASCADE ON UPDATE CASCADE,
-<<<<<<< HEAD
-  ADD CONSTRAINT `nauczyciele_klasa_przedmiot_ibfk_2` FOREIGN KEY (`id_oceny`) REFERENCES `oceny` (`id_oceny`) ON DELETE CASCADE ON UPDATE CASCADE,
-=======
->>>>>>> Patryk
   ADD CONSTRAINT `nauczyciele_klasa_przedmiot_ibfk_3` FOREIGN KEY (`id_przedmiotu`) REFERENCES `przedmioty` (`id_przedmiotu`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `nauczyciele_klasa_przedmiot_ibfk_4` FOREIGN KEY (`id_użytkownika`) REFERENCES `uzytkownicy` (`id_uzytkownika`) ON DELETE CASCADE ON UPDATE CASCADE;
 
@@ -362,12 +282,8 @@ ALTER TABLE `nauczyciele_klasa_przedmiot`
 --
 ALTER TABLE `oceny`
   ADD CONSTRAINT `oceny_ibfk_1` FOREIGN KEY (`id_przemiotu`) REFERENCES `przedmioty` (`id_przedmiotu`) ON DELETE CASCADE ON UPDATE CASCADE,
-<<<<<<< HEAD
-  ADD CONSTRAINT `oceny_ibfk_2` FOREIGN KEY (`id_uzytkownika`) REFERENCES `uzytkownicy` (`id_uzytkownika`) ON DELETE CASCADE ON UPDATE CASCADE;
-=======
   ADD CONSTRAINT `oceny_ibfk_2` FOREIGN KEY (`id_uzytkownika`) REFERENCES `uzytkownicy` (`id_uzytkownika`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `oceny_ibfk_3` FOREIGN KEY (`id_klasy`) REFERENCES `klasa` (`id_klasy`) ON DELETE CASCADE ON UPDATE CASCADE;
->>>>>>> Patryk
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
